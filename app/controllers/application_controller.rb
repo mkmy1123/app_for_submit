e8d820ca5553ae0616ab6d383f4bd6627fff7dfd
Maki Miyagawa <mkmy1123@gmail.com>
2020-10-26 23:11:58 +0900

    i18nの実装方法を見直しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :set_locale
+  around_action :switch_locale
 
-  private
-  def set_locale
-    if %w(ja en).include?(cookies[:locale])
-      I18n.locale = cookies[:locale]
-    end
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
