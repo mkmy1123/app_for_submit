ebd3de4e886f465a28606cdf21e71507a87a8328
Maki Miyagawa <mkmy1123@gmail.com>
2020-10-31 07:51:40 +0900

    3回目のレビューに対応しました

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,16 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      enable_starttls_auto: true,
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: Rails.application.credentials.google[:email],
+      password: Rails.application.credentials.google[:password],
+      authentication: "plain"
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
